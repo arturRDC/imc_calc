6a157a283dfad0d6ae45496f9719a748cde70d59
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2022-09-14 18:43:39 -0300

    Fixed bug

--- lib/main.dart
+++ lib/main.dart
@@ -35,13 +35,16 @@ class _HomeState extends State<Home> {
   void _calcular() {
     setState(() {
       double peso = double.parse(pesoController.text);
-      double altura = double.parse(alturaController.text);
+      double altura = double.parse(alturaController.text) / 100;
       double imc = peso / (altura * altura);
+      debugPrint("imc: $imc");
       if (imc < 18.6) {
         _textInfo = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 18.6 && imc < 24.9) {
         _textInfo = "Peso ideal (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 24.9 && imc < 29.9) {
+        _textInfo = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
         _textInfo = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 34.9 && imc < 39.9) {
         _textInfo = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
@@ -67,6 +70,7 @@ class _HomeState extends State<Home> {
       body: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
           child: Form(
+            key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
@@ -111,21 +115,20 @@ class _HomeState extends State<Home> {
                     bottom: 10.0,
                   ),
                   child: ButtonTheme(
-                    height: 50.0,
-                    highlightColor: Colors.amber,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) _calcular();
-                      },
-                      child: Text(
-                        "Calcular",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25.0,
+                      height: 50.0,
+                      highlightColor: Colors.amber,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) _calcular();
+                        },
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25.0,
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
+                      )),
                 ),
                 Text(
                   _textInfo,
